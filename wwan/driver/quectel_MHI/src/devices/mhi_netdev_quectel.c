ce088ad2efcadfb65cd2cc028f81497d49aca59a
Sean Khan <datapronix@protonmail.com>
2025-06-03 15:27:22 -0400

    wwan: Make rmnet-nss into a separate module
    Allows the rmnet-nss module to be built independently of the Quectel
    driver.
    
    If `rmnet-nss` is selected, the Quectel drivers (wwan/mhi) will enable
    NSS offload support support.
    
    NOTE: NSS offload is completely broken on IPQ5018. Do NOT enable this
    module.
    
    The Quectel drivers and rmnet-nss module are compile and load tested
    (insmod) ONLY. There is NO guarantee or support outside of build errors.
    
    Signed-off-by: Sean Khan <datapronix@protonmail.com>

--- wwan/driver/quectel_MHI/src/devices/mhi_netdev_quectel.c
+++ wwan/driver/quectel_MHI/src/devices/mhi_netdev_quectel.c
@@ -31,8 +31,6 @@
 #include <net/tcp.h>
 #include <linux/usb/cdc.h>
 
-#include <rmnet_nss.h>
-
 //#define CONFIG_IPQ5018_RATE_CONTROL //Only used with spf11.5 for IPQ5018
 #if defined(CONFIG_IPQ5018_RATE_CONTROL)
 //#include <linux/jiffies.h>
@@ -59,19 +57,26 @@ static bool netdev_is_rx_handler_busy(struct net_device *dev)
 }
 #endif
 
-static struct rmnet_nss_cb __read_mostly *nss_cb = NULL;
+#ifdef CONFIG_QCA_NSS_DRV
 #if defined(CONFIG_PINCTRL_IPQ807x) || defined(CONFIG_PINCTRL_IPQ5018) || defined(CONFIG_PINCTRL_IPQ8074)
-//#ifdef CONFIG_RMNET_DATA //spf12.x have no macro defined, just for spf11.x
-#define CONFIG_QCA_NSS_DRV
 #define CONFIG_USE_RMNET_DATA_FOR_SKIP_MEMCPY
+#include <rmnet_nss.h>
+//#ifdef CONFIG_RMNET_DATA //spf12.x have no macro defined, just for spf11.x
 /* define at qca/src/linux-4.4/drivers/net/ethernet/qualcomm/rmnet/rmnet_config.c */ //for spf11.x
 /* define at qsdk/qca/src/datarmnet/core/rmnet_config.c */ //for spf12.x
 /* set at qsdk/qca/src/data-kernel/drivers/rmnet-nss/rmnet_nss.c */
 /* need add DEPENDS:= kmod-rmnet-core in feeds/makefile */
 extern struct rmnet_nss_cb *rmnet_nss_callbacks __rcu __read_mostly;
-//#endif
+#endif
+#else
+struct rmnet_nss_cb {
+	int (*nss_create)(void *dev);
+	int (*nss_free)(void *dev);
+	int (*nss_tx)(void *skb);
+};
 #endif
 
+static struct rmnet_nss_cb __read_mostly *nss_cb = NULL;
 
 int mhi_netdev_use_xfer_type_dma(unsigned chan)
 {
